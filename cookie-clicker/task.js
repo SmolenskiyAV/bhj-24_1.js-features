0044cf065b8b5421a5a3408c2a008907f624d235
SmolenskiyAV <JoJo_666@list.ru>
2021-12-13 13:06:32 +0700

    Версия 2.0. Задача 1.2 выполнена

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -1,1 +1,23 @@
 // ### Task 1.2 ###
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("clicker__counter");
+const speed = document.getElementById("clicker__speed");
+
+let counterValue = 0;
+let speedValue = 0;
+let pressClickTime;
+let releaseClickTime;
+
+cookie.addEventListener("mousedown", function() { // событие по нажатию левой кн.мыши
+    pressClickTime = new Date();
+    cookie.width = 300;
+    counterValue++;
+    clicker__counter.textContent = counterValue;
+});
+
+cookie.addEventListener("mouseup", function() {  // событие по отжатию левой кн.мыши
+    releaseClickTime = new Date();
+    speedValue = (1000 / (releaseClickTime - pressClickTime)).toFixed(2);
+    cookie.width = 200;
+    clicker__speed.textContent = speedValue;
+});
