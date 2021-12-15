8ec3ea98fe1490fb1a6d81038be54b072ff64977
SmolenskiyAV <JoJo_666@list.ru>
2021-12-15 11:12:17 +0700

    Версия 3.3 Корректировка вычисления скорости клика

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -5,19 +5,19 @@ const speed = document.getElementById("clicker__speed");
 
 let counterValue = 0;
 let speedValue = 0;
-let pressClickTime;
-let releaseClickTime;
+let nowClickTime;
+let previousClickTime = new Date();
 
 cookie.addEventListener("mousedown", function() { // событие по нажатию левой кн.мыши
-    pressClickTime = new Date();
+    nowClickTime = new Date();
     cookie.width = 300;
     counterValue++;
     clicker__counter.textContent = counterValue;
+    speedValue = (1000 / (nowClickTime - previousClickTime)).toFixed(2); // вычисление скорости кликов в секунду
 });
 
 cookie.addEventListener("mouseup", function() {  // событие по отжатию левой кн.мыши
-    releaseClickTime = new Date();
-    speedValue = (1000 / (releaseClickTime - pressClickTime)).toFixed(2); // вычисление скорости клика за секунду
     cookie.width = 200;
     clicker__speed.textContent = speedValue;
+    previousClickTime = nowClickTime;
 });
